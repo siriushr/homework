6f6d24ca86aa04587fcadda8aaed45561d790c20
siriushr <siriuslemon@gmail.com>
2015-07-29 18:43:35 +0800

    晶晶赴约会

--- 晶晶赴约会.cpp
+++ 晶晶赴约会.cpp
@@ -1,14 +1,22 @@
+//
+//  main.cpp
+//  晶晶赴约会
+//
+//  Created by 黄然 on 15/7/28.
+//  Copyright (c) 2015年 黄然. All rights reserved.
+//
+
 #include <iostream>
 using namespace std;
 
 int main() {
-	int date = 0;
-	cin >> date;
-	if (date == 1 || date == 3 || date == 5) {
-		cout << "NO" << endl;
-	}
-	else {
-		cout << "YES" << endl;
-	}
-	return 0;
+    int date;
+    cin >> date;
+    if (date == 1 || date == 3 || date == 5) {
+        cout << "NO" << endl;
+    }
+    else {
+        cout << "YES" << endl;
+    }
+    return 0;
 }
